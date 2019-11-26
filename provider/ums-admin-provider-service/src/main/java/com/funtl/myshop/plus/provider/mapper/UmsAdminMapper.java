d73b61f019aee3184ec1b330070be35c65f66ff4
Administrator <tangyangjun88@163.com>
2019-11-26 18:20:35 +0800

    update project

--- provider/ums-admin-provider-service/src/main/java/com/funtl/myshop/plus/provider/mapper/UmsAdminMapper.java
+++ provider/ums-admin-provider-service/src/main/java/com/funtl/myshop/plus/provider/mapper/UmsAdminMapper.java
@@ -1,7 +1,7 @@
 package com.funtl.myshop.plus.provider.mapper;
 
 import com.funtl.myshop.plus.provider.domain.UmsAdmin;
-import tk.mybatis.mapper.Mapper;
+import tk.mybatis.mapper.MyMapper;
 
-public interface UmsAdminMapper extends Mapper<UmsAdmin> {
+public interface UmsAdminMapper extends MyMapper<UmsAdmin> {
 }
